0fda9b08895f3591f32e582107f0327a4c22d92f
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-03-10 12:22:04 -0500

    Fixed screen object to graphWindow object to avoid confusion script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ idTree.canvas.style.width = `calc(${window.innerHeight / 2}px)`;
 idTree.canvas.style.height = `calc(${window.innerHeight / 2}px)`;
 idTree.screenSize.value = window.innerHeight / 2;
 
-let screen = {
+let graphWindow = {
     size: +idTree.screenSize.value,
     edge: this.size / 2,
     windowX: +idTree.windowX.value,
@@ -20,24 +20,24 @@ let screen = {
 }
 
 function regenerateGrid() {
-    for (let i = 0; i <= edge; i += (edge) / screen.windowX) {
-        idTree.canvas.innerHTML += `<path d="M${i},${-edge} L${i},${edge}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
+    for (let i = 0; i <= graphWindow.edge; i += (graphWindow.edge) / graphWindow.windowX) {
+        idTree.canvas.innerHTML += `<path d="M${i},${-graphWindow.edge} L${i},${graphWindow.edge}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
     }
 
-    for (let i = 0; i >= -edge; i -= (edge) / screen.windowX) {
-        idTree.canvas.innerHTML += `<path d="M${i},${-edge} L${i},${edge}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
+    for (let i = 0; i >= -graphWindow.edge; i -= (graphWindow.edge) / graphWindow.windowX) {
+        idTree.canvas.innerHTML += `<path d="M${i},${-graphWindow.edge} L${i},${graphWindow.edge}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
     }
 
-    for (let i = 0; i <= edge; i += (edge) / screen.windowY) {
-        idTree.canvas.innerHTML += `<path d="M${-edge},${i} L${edge},${i}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
+    for (let i = 0; i <= graphWindow.edge; i += (graphWindow.edge) / graphWindow.windowY) {
+        idTree.canvas.innerHTML += `<path d="M${-graphWindow.edge},${i} L${graphWindow.edge},${i}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
     }
 
-    for (let i = 0; i >= -edge; i -= (edge) / screen.windowY) {
-        idTree.canvas.innerHTML += `<path d="M${-edge},${i} L${edge},${i}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
+    for (let i = 0; i >= -graphWindow.edge; i -= (graphWindow.edge) / graphWindow.windowY) {
+        idTree.canvas.innerHTML += `<path d="M${-graphWindow.edge},${i} L${graphWindow.edge},${i}" style="stroke: #808080; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
     }
 
-    idTree.canvas.innerHTML = idTree.canvas.innerHTML + `<path d="M${-edge},0 L${edge},0" style="stroke: White; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
-    idTree.canvas.innerHTML = idTree.canvas.innerHTML + `<path d="M0,${edge} L0,${-edge}" style="stroke: White; fill: none; stroke-width: 1;" transform="translate(${edge}, ${edge})" />`;
+    idTree.canvas.innerHTML = idTree.canvas.innerHTML + `<path d="M${-graphWindow.edge},0 L${graphWindow.edge},0" style="stroke: White; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
+    idTree.canvas.innerHTML = idTree.canvas.innerHTML + `<path d="M0,${graphWindow.edge} L0,${-graphWindow.edge}" style="stroke: White; fill: none; stroke-width: 1;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})" />`;
 }
 
 function evaluateYValue(value) {
@@ -51,29 +51,29 @@ regenerateGrid();
 oninput = function () {
     d = ``;
 
-    screen = {
+    graphWindow = {
         size: +idTree.screenSize.value,
         edge: this.size / 2,
         windowX: +idTree.windowX.value,
         windowY: +idTree.windowY.value
     }
 
-    if (screen.size === "") {
+    if (graphWindow.size === "") {
         return;
     }
 
-    idTree.canvas.style.width = screen.size + "px";
-    idTree.canvas.style.height = screen.size + "px";
+    idTree.canvas.style.width = graphWindow.size + "px";
+    idTree.canvas.style.height = graphWindow.size + "px";
 
     equations = idTree.input.value.split("\n");
 
     idTree.canvas.innerHTML = "";
 
-    if (screen.windowX === "") {
+    if (graphWindow.windowX === "") {
         return;
     }
 
-    if (screen.windowY === "") {
+    if (graphWindow.windowY === "") {
         return;
     }
 
@@ -90,22 +90,22 @@ oninput = function () {
             break;
         }
 
-        xvalue = -screen.windowX;
+        xvalue = -graphWindow.windowX;
         yvalue = Math.round(evaluateYValue(xvalue) * 100000) / 100000;
 
         if (abs(yvalue) === Infinity || isNaN(yvalue)) {
             while (abs(yvalue) === Infinity || isNaN(yvalue)) {
                 xvalue += 0.01;
                 yvalue = Math.round(evaluateYValue(xvalue) * 100000) / 100000;
-                if (xvalue > screen.WindowX) {
+                if (xvalue > graphWindow.WindowX) {
                     break;
                 }
             }
         }
 
-        d += `M${(screen.size / (screen.windowX * 2)) * xvalue},${(screen.size / (screen.windowY * 2)) * yvalue} \n`;
+        d += `M${(graphWindow.size / (graphWindow.windowX * 2)) * xvalue},${(graphWindow.size / (graphWindow.windowY * 2)) * yvalue} \n`;
 
-        for (xvalue = -screen.windowX; xvalue <= screen.windowX; xvalue += 0.01) {
+        for (xvalue = -graphWindow.windowX; xvalue <= graphWindow.windowX; xvalue += 0.01) {
             xvalue = Math.round(xvalue * 100) / 100;
             yvalue = Math.round(evaluateYValue(xvalue) * 100000) / 100000;
 
@@ -114,19 +114,19 @@ oninput = function () {
                     while (abs(yvalue) === Infinity || isNaN(yvalue)) {
                         xvalue += 0.01;
                         yvalue = Math.round(evaluateYValue(xvalue) * 100000) / 100000;
-                        if (xvalue > screen.WindowX) {
+                        if (xvalue > graphWindow.WindowX) {
                             break;
                         }
                     }
 
-                    d += `M${(screen.size / (screen.windowX * 2)) * xvalue},${(screen.size / (screen.windowY * 2)) * yvalue} \n`;
+                    d += `M${(graphWindow.size / (graphWindow.windowX * 2)) * xvalue},${(graphWindow.size / (graphWindow.windowY * 2)) * yvalue} \n`;
                 } else {
-                    d += `L${(screen.size / (screen.windowX * 2)) * xvalue},${(screen.size / (screen.windowY * 2)) * yvalue} \n`;
+                    d += `L${(graphWindow.size / (graphWindow.windowX * 2)) * xvalue},${(graphWindow.size / (graphWindow.windowY * 2)) * yvalue} \n`;
                     points.toString(j)[xvalue] = -yvalue;
                 }
             }
         }
 
-        idTree.canvas.innerHTML += `<path id="function" d="${d}" style="stroke: White; fill: none; stroke-width: 3;" transform="translate(${edge}, ${edge})"/>`;
+        idTree.canvas.innerHTML += `<path id="function" d="${d}" style="stroke: White; fill: none; stroke-width: 3;" transform="translate(${graphWindow.edge}, ${graphWindow.edge})"/>`;
     }
 }
