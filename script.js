602ba8dd8e443f90ae18beef4135abcc7d1b9cc8
CaretParrot <wopow29@gmail.com>
2025-02-01 17:31:00 -0600

    Improved functionality

--- script.js
+++ script.js
@@ -6,26 +6,29 @@ let d;
 let j;
 let canvas = document.getElementById("graph");
 let paint = canvas.getContext("2d");
+let domain = 10;
+let range = 10;
 
 window.onload = function () {
     paint.translate(canvas.width / 2, canvas.height / 2);
-    drawGrid();
+
+    drawGraph();
 };
 
 function drawGrid() {
     paint.beginPath();
     paint.strokeStyle = "gray";
 
-    for (let i = -canvas.height / 2; i <= canvas.height / 2; i += canvas.height / (10 * 2)) {
+    for (let i = -canvas.height / 2; i <= canvas.height / 2; i += canvas.width / (range * 2)) {
         paint.moveTo(-canvas.width / 2, i);
         paint.lineTo(canvas.width / 2, i);
     }
 
-    for (let i = -canvas.width / 2; i <= canvas.width / 2; i += canvas.width / (10 * 2)) {
+    for (let i = -canvas.width / 2; i <= canvas.width / 2; i += canvas.height / (domain * 2)) {
         paint.moveTo(i, -canvas.height / 2);
         paint.lineTo(i, canvas.height / 2);
     }
-    
+
     paint.stroke();
 }
 
@@ -33,26 +36,31 @@ oninput = function (event) {
     lines = document.getElementById("input").value.split("\n");
     equations = [];
     drawGraph();
+}
+
+function drawGraph() {
+    paint.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+
+    drawGrid();
+
+    paint.beginPath();
+    paint.strokeStyle = "white";
+
     for (let i = 0; i < lines.length; i++) {
         equations.push(new mathPlus.MathFunction(lines[i], "x"));
     }
 
     for (let i = 0; i < equations.length; i++) {
         try {
-            paint.moveTo(-canvas.width / 2, equations[i].evaluate(-canvas.width / 2));
-            for (let xVal = -canvas.width / 2; xVal <= canvas.width / 2; xVal += 0.1) {
-                paint.lineTo(xVal, equations[i].evaluate(xVal));
+            paint.moveTo(-canvas.width / 2, -equations[i].evaluate(-canvas.width / 2));
+            for (let xVal = -domain; xVal <= domain; xVal += 0.01) {
+                paint.lineTo(xVal * (canvas.width / domain), -equations[i].evaluate(xVal * (canvas.width / domain)));
             }
+            paint.stroke();
         } catch (error) {
             console.log(`Error: ${error}`);
         }
     }
-}
-
-function drawGraph() {
-    paint.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    
-    drawGrid();
 }
 
 function copyValue() {
