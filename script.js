5fae2fa1cf5d5429b1f03b77b81f86913653dca6
CaretParrot <rishi.venkatesh720@gmail.com>
2025-12-17 16:23:11 -0500

    Removed errors

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let lines = [];
+/**
+ * @type {any}
+ */
 let equations = [];
 let points = [];
 let output;
 let d;
 let j;
-let canvas = document.getElementById("graph");
-let paint = canvas.getContext("2d");
-let domain = 10;
-let range = 10;
 
-window.onload = function () {
-    paint.translate(canvas.width / 2, canvas.height / 2);
-    drawGrid();
-};
+let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("graph"));
+let paint = /** @type {CanvasRenderingContext2D} */ (canvas.getContext("2d"));
+let domain = /** @type {HTMLInputElement} */ (document.getElementById("domain"));
+let range = /** @type {HTMLInputElement} */ (document.getElementById("range"));
+
+let functionSelect = /** @type {HTMLInputElement} */ (document.getElementById("functionSelect"));
+let xInput = /** @type {HTMLInputElement} */ (document.getElementById("xInput"));
+let xLabel = /** @type {HTMLLabelElement} */ (document.getElementById("xLabel"));
+let input = /** @type {HTMLInputElement} */ (document.getElementById("input"));
+
+paint.translate(canvas.width / 2, canvas.height / 2);
+drawGrid();
 
 function drawGrid() {
     paint.beginPath();
@@ -20,12 +27,12 @@ function drawGrid() {
 
     paint.strokeStyle = "gray";
 
-    for (let i = -canvas.height / 2; i <= canvas.height / 2; i += canvas.width / (range * 2)) {
+    for (let i = -canvas.height / 2; i <= canvas.height / 2; i += canvas.width / (+range.value * 2)) {
         paint.moveTo(-canvas.width / 2, i);
         paint.lineTo(canvas.width / 2, i);
     }
 
-    for (let i = -canvas.width / 2; i <= canvas.width / 2; i += canvas.height / (domain * 2)) {
+    for (let i = -canvas.width / 2; i <= canvas.width / 2; i += canvas.height / (+domain.value * 2)) {
         paint.moveTo(i, -canvas.height / 2);
         paint.lineTo(i, canvas.height / 2);
     }
@@ -44,15 +51,11 @@ function drawGrid() {
 }
 
 oninput = function (event) {
-    domain = document.getElementById("domain").value || 10;
-    range = document.getElementById("range").value || 10;
     drawGraph();
-
-    document.getElementById("functionSelect").max = lines.length - 1;
-
-    document.getElementById("xInput").min = -domain;
-    document.getElementById("xInput").max = domain;
-    document.getElementById("xLabel").innerHTML = `(${+document.getElementById("xInput").value}, ${equations[+document.getElementById("functionSelect").value].evaluate(+document.getElementById("xInput").value)})`;
+    functionSelect.max = (lines.length - 1).toString();
+    xInput.min =(-domain.value).toString();
+    xInput.max = domain.value;
+    xLabel.innerHTML = `(${+xInput.value}, ${equations[+functionSelect.value].evaluate(+xInput.value)})`;
 }
 
 function drawGraph() {
@@ -61,12 +64,13 @@ function drawGraph() {
     points = [];
     drawGrid();
 
-    lines = document.getElementById("input").value.split("\n");
+    lines = input.value.split("\n");
     equations = [];
 
     for (let i = 0; i < lines.length; i++) {
         if (lines[i] !== "") {
-            equations.push(new mathPlus.MathFunction(lines[i], "x"));
+            // @ts-ignore
+            equations.push(new MathFunction(lines[i], "x"));
         }
     }
 
@@ -78,14 +82,14 @@ function drawGraph() {
         try {
             let xVal = Math.round(-domain * 100) / 100;
             let yVal = -equations[i].evaluate(xVal);
-            paint.moveTo(xVal * canvas.width / domain / 2, yVal * canvas.height / range / 2);
+            paint.moveTo(xVal * canvas.width / +domain.value / 2, yVal * canvas.height / +range.value / 2);
 
-            for (xVal = -domain; xVal <= domain; xVal += 0.01) {
+            for (xVal = -domain; xVal <= +domain.value; xVal += 0.01) {
                 xVal = Math.round(xVal * 100) / 100;
                 yVal = -equations[i].evaluate(xVal);
                 if (isFinite(-equations[i].evaluate(xVal))) {
-                    paint.lineTo(xVal * canvas.width / domain / 2, yVal * canvas.height / range / 2);
-                    points.push([xVal * canvas.width / domain / 2, yVal]);
+                    paint.lineTo(xVal * canvas.width / +domain.value / 2, yVal * canvas.height / +range.value / 2);
+                    points.push([xVal * canvas.width / +domain.value / 2, yVal]);
                 } else {
                     paint.stroke();
                 }
@@ -99,5 +103,5 @@ function drawGraph() {
 }
 
 function copyFunctions() {
-    navigator.clipboard.writeText(document.getElementById("input").value);
+    navigator.clipboard.writeText(input.value);
 }
